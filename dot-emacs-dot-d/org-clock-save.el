676d8519a5761fbddf7b4cd3bfba566e847a2308
Vincent Emanuele <vincent.emanuele@wellcentive.com>
2014-10-26 20:15:43 -0400

    Removing temporary clock file used by org mode

--- dot-emacs-dot-d/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - athens at <2014-10-26 Sun 10:09>
-(setq stored-clock-history '(("/Users/vaemanuele/org/todo.org" . 338288) ("/Users/vaemanuele/org/wellcentive/wellcentive.org" . 856052) ("/Users/vaemanuele/org/wellcentive/wellcentive.org" . 855757) ("/Users/vaemanuele/org/todo.org" . 338204) ("/Users/vaemanuele/org/wellcentive/wellcentive.org" . 855859)))
